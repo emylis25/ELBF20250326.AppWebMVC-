30bd77a92cdf0b8b067664e208936f3231ecb535
Emily Lisbeth Bolaño Flores <2429841@clases.edu.sv>
2025-03-26 07:49:38 -0600

    Agregar archivos de proyecto.

---
+++ ELBF20250326.AppWebMVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using ELBF20250326.AppWebMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ELBF20250326.AppWebMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
