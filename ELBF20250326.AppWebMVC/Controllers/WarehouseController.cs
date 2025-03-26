dd1c9882036c4e02696941abe27e6f864c8c8a5f
Emily Lisbeth Bolaño Flores <2429841@clases.edu.sv>
2025-03-26 09:55:18 -0600

    Modificacion en el CRUD de Bodega. y algunos camios en el layout.

--- ELBF20250326.AppWebMVC/Controllers/WarehouseController.cs
+++ ELBF20250326.AppWebMVC/Controllers/WarehouseController.cs
@@ -6,9 +6,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ELBF20250326.AppWebMVC.Models;
+using X.PagedList.Extensions;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ELBF20250326.AppWebMVC.Controllers
 {
+    //[Authorize(Roles = "GERENTE")]
     public class WarehouseController : Controller
     {
         private readonly Test20250326DbContext _context;
@@ -19,9 +22,30 @@ namespace ELBF20250326.AppWebMVC.Controllers
         }
 
         // GET: Warehouse
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? searchWarehouseId, int? page, int topRegistro = 10)
         {
-            return View(await _context.Warehouses.ToListAsync());
+            var warehouses = await _context.Warehouses
+                .Select(w => new { w.Id, w.WarehouseName })
+                .ToListAsync();
+
+            ViewBag.WarehouseList = new SelectList(warehouses, "Id", "WarehouseName");
+
+            var query = _context.Warehouses.AsQueryable();
+
+            if (searchWarehouseId.HasValue && searchWarehouseId > 0)
+            {
+                query = query.Where(w => w.Id == searchWarehouseId.Value);
+            }
+
+            ViewBag.SearchWarehouseId = searchWarehouseId;
+            ViewBag.TopRegistro = topRegistro;
+
+            int pageNumber = page ?? 1;
+
+            var warehouseList = await query.ToListAsync();
+            var paginatedList = warehouseList.ToPagedList(pageNumber, topRegistro);
+
+            return View(paginatedList);
         }
 
         // GET: Warehouse/Details/5
--- ELBF20250326.AppWebMVC/Models/Warehouse.cs
+++ ELBF20250326.AppWebMVC/Models/Warehouse.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ELBF20250326.AppWebMVC.Models;
 
 public partial class Warehouse
 {
     public int Id { get; set; }
-
+    [Required(ErrorMessage = "El nombre de bodega es obligatorio.")]
     public string WarehouseName { get; set; } = null!;
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
